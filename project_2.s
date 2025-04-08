256d31694ba84878330bdb1d536046f898698de3
Tahmauri <tmbo718@gmail.com>
2025-04-07 22:38:35 -0400

    Added character max, NULL message, and colon character

---
+++ project_2.s
@@ -0,0 +1,6 @@
+.data
+SpaceInput: .space 1002     #max amount of characters plus newline and null
+null_msg: .asciiz "NULL"    #NULL message for printing
+semicolon: .asciiz ";"      #Colon for separating
+
+.text
