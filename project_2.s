e1166023fc54dc7f6e872ee0930571b973e693ea
Tahmauri <tmbo718@gmail.com>
2025-04-07 22:58:37 -0400

    Added N and M

--- project_2.s
+++ project_2.s
@@ -4,3 +4,13 @@ null_msg: .asciiz "NULL"    #NULL message for printing
 semicolon: .asciiz ";"      #Colon for separating
 
 .text
+.globl main
+
+main:
+
+    li $t0, 32  #Hardcoded N, stored into $t0
+    li $t1, 10  #stores 10 into $t1
+
+    sub $t2, $t0, $t1   #M = N - 10
+
+    
