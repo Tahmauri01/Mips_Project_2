e23a95a2f15fa23f7a15a8ebafb37339328e4ad7
Tahmauri <tmbo718@gmail.com>
2025-04-07 23:22:10 -0400

    Added 'a' and 'A'. Also added a cap for lowercase characters

--- project_2.s
+++ project_2.s
@@ -13,4 +13,9 @@ main:
 
     sub $t2, $t0, $t1   #M = N - 10
 
-    
+    li $t3, 0x61 #stores 'a' into $t3
+    add $t4, $t3, $t2 #$t4 = 'a' + M
+    addi $t4, $t4, -1 #cap for lowercase, $t4 = 'a' + M - 1
+
+    li $t5, 0x41 #stores 'A' int $t5
+    add $t6, $t5, $t2 #$t6 = 'A" + M
